ca4b4737974d627ebcd52c00ea0c32b88ad719ed
kimiaki704 <kimiaki704@gmail.com>
2020-06-10 14:40:52 +0900

    Init Xcode

---
+++ MoreReadSample/MoreReadViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MoreReadViewController.swift
+//  MoreReadSample
+//
+//  Created by 鈴木 公章 on 2020/06/10.
+//  Copyright © 2020 kimioman. All rights reserved.
+//
+
+import UIKit
+
+class MoreReadViewController: UIViewController {}
+
