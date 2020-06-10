39ac9d6d1e695f9d07d996a0c2dc517bcea87f9d
kimiaki704 <kimiaki704@gmail.com>
2020-06-10 12:35:13 +0900

    Initial Commit

---
+++ MoreReadSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoreReadSample
+//
+//  Created by 鈴木 公章 on 2020/06/10.
+//  Copyright © 2020 kimioman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
